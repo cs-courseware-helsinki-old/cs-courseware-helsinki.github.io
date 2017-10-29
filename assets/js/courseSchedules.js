5725e71e2076c692146b13ebeb9bfe94bcbb0849
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2017-10-29 12:16:46 +0200

    Schedules from json etc

---
+++ assets/js/courseSchedules.js
@@ -0,0 +1,43 @@
+const courseAPI_url = "courseSchedules_sample.json";  
+let courseSchedules = "";
+
+function loadCourseSchedules() {
+  let xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+      courseSchedules = JSON.parse(this.responseText);
+      }
+  };
+
+  xmlhttp.open("GET", courseAPI_url, true);
+  xmlhttp.send();
+}
+
+function getCoursesByPeriod(givenPeriod) {
+  return courseSchedules.find(item => {return item.period == givenPeriod}).courses;
+}
+
+function highlightCoursesByPeriod(period) {
+  let temp = getCoursesByPeriod(period);
+  highlightCourses(temp);
+}
+
+function resetHighlights() {
+  $(".course-row").each(function(){
+    $(this).css("background-color", "");
+  })
+}
+
+function highlightCourses(courseCodeList) {
+  resetHighlights();
+  $.each( courseCodeList, function(i, val) {
+    $("#" + val).css("background-color", "#FFF294");
+  })
+}
+
+function main() {
+  loadCourseSchedules()
+}
+
+main()
